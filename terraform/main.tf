802d52a5cc34dd1d65da60100bf9adc6bfdc467a
Alexandr Vinogradov <a.vinogradov@ve-lab.com>
2018-04-20 10:38:29 +0300

    добавлены комментарии по выполнению первого задания со звёздочкой

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,14 @@ provider "google" {
   region  = "${var.region}"
 }
 
+resource "google_compute_project_metadata" "default" {
+  metadata {
+    ssh-keys = "sologm:${file("${var.public_key_path}")} appuser2:${file("${var.public_key_path}")} appuser3:${file("${var.public_key_path}")}"
+  }
+}
+
 resource "google_compute_instance" "app" {
-  name         = "reddit-app-tf"
+  name         = "reddit-app"
   machine_type = "g1-small"
   zone         = "${var.zone}"
   tags         = ["reddit-app"]
