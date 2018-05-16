8e1f8940ce3c5ad16eccd639bbe7284ba4791a77
Alexandr Vinogradov <a.vinogradov@ve-lab.com>
2018-05-16 12:48:38 +0300

    единый плейбук reddit_app.yml для настройки хостов и деплоя приложений

--- ansible/gce_inventory.sh
+++ ansible/gce_inventory.sh
@@ -4,6 +4,7 @@ if [[ $1 == "--list" ]]
 then
 app_external_ip=$(gcloud compute instances describe reddit-app --zone="europe-west1-d" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
 db_external_ip=$(gcloud compute instances describe reddit-db --zone="europe-west1-d" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
+db_internal_ip=$(gcloud compute instances describe reddit-db --zone="europe-west1-d" --format="value(networkInterfaces[0].networkIP)")
 
 cat << EndOfJSON
 {
@@ -23,7 +24,8 @@ cat << EndOfJSON
                 "ansible_host": "${app_external_ip}"
             },
             "dbserver": {
-                "ansible_host": "${db_external_ip}"
+                "ansible_host": "${db_external_ip}",
+                "internal_ip": "${db_internal_ip}"
             }
         }
     }
